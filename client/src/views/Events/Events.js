f79fd2e6cb5c9861db9872cc0e8f8e97d2ef5fd8
devagrawal09 <devagrawal09@gmail.com>
2019-11-02 22:14:59 -0400

    events page link

--- client/src/views/Events/Events.js
+++ client/src/views/Events/Events.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 class Events extends Component {
 	events() {
 		return [
-			{ title: 'Hackathon #1', text: 'Best Hackathon Ever!' },
-			{ title: 'Hackathon #2', text: 'Worst Hackathon Ever!' }
+			{ id: "blah", title: 'Hackathon #1', text: 'Best Hackathon Ever!' },
+			{ id: "blah2", title: 'Hackathon #2', text: 'Worst Hackathon Ever!' }
 		]
 	}
-  render() {
-    return (
-      <div className="animated fadeIn">
-        <ListGroup>
-					{ this.events().map( e=> 
-							(<ListGroupItem>
-								<ListGroupItemHeading>{e.title}</ListGroupItemHeading>
-								<ListGroupItemText>{e.text}</ListGroupItemText>
-							</ListGroupItem>)
-						)
-					}
+
+	render() {
+		return (
+			<div className="animated fadeIn">
+				<ListGroup>
+					{this.events().map(e =>
+						(<ListGroupItem>
+							<ListGroupItemHeading>
+								<Link to={`/event/${e.id}`}>{e.title}</Link>
+							</ListGroupItemHeading>
+							<ListGroupItemText>{e.text}</ListGroupItemText>
+						</ListGroupItem>)
+					)}
 				</ListGroup>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default Events;

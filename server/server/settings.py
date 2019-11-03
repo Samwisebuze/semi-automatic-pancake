a0d28a3f5fcc937642774322576bfe06b6787b93
Samuel Buzas <buzassg@mail.uc.edu>
2019-11-02 21:55:14 -0400

    LIVE!

--- server/server/settings.py
+++ server/server/settings.py
@@ -72,20 +72,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', ''),
-        'USER': os.environ.get('DB_USER', ''),
-        'PASSWORD': os.environ.get('DB_PASS', ''),
-        'HOST': 'postgres',
+        'NAME': 'xyktgzgi',
+        'USER': 'xyktgzgi',
+        'PASSWORD': 'YOBjhMbs2d2ndqCdvG975FVk10t03A83',
+        'HOST': 'salt.db.elephantsql.com',
         'PORT': 5432,
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
